8f37660d7c42f24d7788c360cfe8efc7b8be951b
NotAccursed <30912628+NotAccursed@users.noreply.github.com>
2019-05-26 00:34:47 +0200

    Credits to: https://stackoverflow.com/a/36125728

--- Simple Licensing/Utils/IDs.cs
+++ Simple Licensing/Utils/IDs.cs
@@ -5,6 +5,8 @@ namespace Simple_Licensing.Utils
 {
 	class IDs
 	{
+
+		//Thanks to: https://stackoverflow.com/a/36125728
 		public static string GetHardDiskSerialNo()
 		{
 			ManagementClass mangnmt = new ManagementClass("Win32_LogicalDisk");
