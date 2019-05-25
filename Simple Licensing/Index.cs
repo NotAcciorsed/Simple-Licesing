1a213c5169da26320853272c529ddde5ca7eee93
NotAccursed <30912628+NotAccursed@users.noreply.github.com>
2019-05-26 00:30:05 +0200

    Add project files.

---
+++ Simple Licensing/Index.cs
@@ -0,0 +1,48 @@
+﻿using Simple_Licensing.Utils;
+using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Simple_Licensing
+{
+	class Index
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Init();
+		}
+
+		public static void Init()
+		{
+			WebClient wc = new WebClient();
+
+			//Getting strings from an online file
+			var result = wc.DownloadString("http://downloader.altervista.org/Simple-Licensing");
+
+			//Checking if the online file contains our HWID
+			if (result.Contains(IDs.GetHardDiskSerialNo()))
+			{
+				setResult("Success!");
+				setColor(ConsoleColor.Green);
+			}
+			else
+			{
+				Clipboard.SetText(IDs.GetHardDiskSerialNo());
+				setResult("You are not whitelisted - HWID COPIED!");
+				setColor(ConsoleColor.Red);
+			}
+			Console.ReadLine();
+		}
+
+		public static void setResult(string result)
+		{
+			Console.WriteLine(result);
+		}
+
+		public static void setColor(ConsoleColor consoleColor)
+		{
+			Console.ForegroundColor = consoleColor;
+		}
+	}
+}
